db71fd285ebfe9e93deae7291313ea5e30f53944
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2023-07-08 16:54:04 +0700

    Update CapesCommand.php

--- src/Vanic/Bloody/Cosmetics/Command/CapesCommand.php
+++ src/Vanic/Bloody/Cosmetics/Command/CapesCommand.php
@@ -17,6 +17,7 @@ class CapesCommand extends Command {
   public function __construct(string $name, Main $plugin, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = []) {
     $this->main = $plugin;
     parent::__construct($name, $description, $usageMessage, $aliases);
+    $this->setPermission("bloodycosmetics.use");
   }
   
   /**
